70d7114205e9c61ef79fb4aa0d2b4c5c341a7d69
Mandar Upasani <mandar1695@gmail.com>
2026-04-07 12:05:54 +0530

    Adding functionality of cleaning input
    All tests OK

--- input_processing.go
+++ input_processing.go
@@ -1,4 +1,17 @@
 package main
 
+import "strings"
+
 func cleanInput(text string) []string {
+	wsCleanedString := strings.Trim(text, " ")
+
+	stringContents := []string{}
+
+	for subString := range strings.SplitSeq(wsCleanedString, " ") {
+		if len(subString) > 0 {
+			stringContents = append(stringContents, strings.ToLower(subString))
+		}
+	}
+
+	return stringContents
 }
