cbc363fc3b7e9351469860238412ab3b9e61e3d8
Mandar Upasani <mandar1695@gmail.com>
2026-04-07 19:26:09 +0530

    adding config struct to contain next and previous URLs
    Updating the function signatures of callback functions to take config
    struct as parameter

--- commands.go
+++ commands.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 )
+
+type config struct {
+	nextURL url.URL
+	prevURL url.URL
+}
 
 var supportedCommands map[string]command = map[string]command{
 	"exit": {
@@ -21,16 +27,16 @@ var supportedCommands map[string]command = map[string]command{
 type command struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(config) error
 }
 
-func commandExit() error {
+func commandExit(c config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(c config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 
