aac46b75deefe7f46159ff292867ed2c738367c4
Mandar Upasani <mandar1695@gmail.com>
2026-04-07 12:04:51 +0530

    Clean input function definition and testing

---
+++ input_processing.go
@@ -0,0 +1,4 @@
+package main
+
+func cleanInput(text string) []string {
+}
---
+++ input_processing_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected []string
+	}{
+}
