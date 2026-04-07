ef27d186414ae91df6b5583e3eccfd1d45c3f4c2
Mandar Upasani <mandar1695@gmail.com>
2026-04-07 14:05:53 +0530

    incorporating command handling in main
    Also adding clean exit on C-D from REPL
    
    Minor refactoring of prompt function

--- main.go
+++ main.go
@@ -10,21 +10,29 @@ func main() {
 	newScan := bufio.NewScanner(os.Stdin)
 
 	for {
-		shellPrompt("Pokedex >")
-		str, err := inputReader(*newScan)
-		if err != nil {
-			fmt.Printf("Error: %v", err)
+		prompt("Pokedex >")
+
+		if !newScan.Scan() {
+			commandExit()
 		}
 
-		cleanInput := cleanInput(str)
+		str := newScan.Text()
 
-		fmt.Printf("Your command was: %s\n", cleanInput[0])
+		cleanedInput := cleanInput(str)
+		command := cleanedInput[0]
+
+		if comm, ok := supportedCommands[command]; ok {
+			comm.callback()
+			if command == "help" {
+				fmt.Printf("\n%s", helpText(supportedCommands))
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+
 	}
 }
 
-	}
-}
-
-func shellPrompt(PS1 string) {
+func prompt(PS1 string) {
 	fmt.Fprint(os.Stdout, PS1+" ")
 }
