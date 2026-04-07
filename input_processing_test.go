5c162fc976169e018c88b868b9d4088b9c0f79fa
Mandar Upasani <mandar1695@gmail.com>
2026-04-07 12:05:23 +0530

    Adding test cases for input cleaning function

--- input_processing_test.go
+++ input_processing_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -10,4 +11,37 @@ func TestCleanInput(t *testing.T) {
 		input    string
 		expected []string
 	}{
+		{
+			name:     "Normal Input",
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			name:     "Multiple words without extra whitespaces",
+			input:    "charmender charizard pickachu",
+			expected: []string{"charmender", "charizard", "pickachu"},
+		},
+		{
+			name:     "Multiple words with extra spaces",
+			input:    "     charmender      charizard       pickachu",
+			expected: []string{"charmender", "charizard", "pickachu"},
+		},
+		{
+			name:     "Multiple words with extra spaces and mixed case",
+			input:    "  chArmEndEr      cHarIzard       PiCkAcHu",
+			expected: []string{"charmender", "charizard", "pickachu"},
+		},
+		{
+			name:     "Single word in uppercase",
+			input:    "HELLO",
+			expected: []string{"hello"},
+		},
+	}
+
+	for _, tc := range testCases {
+		actual := cleanInput(tc.input)
+		if !reflect.DeepEqual(actual, tc.expected) {
+			t.Errorf("TEST: %s FAILED:\ncleanInput(%q) = %q; expected %q", tc.name, tc.input, actual, tc.expected)
+		}
+	}
 }
